e3eee141c7d4a7c7b3db3457c5e6d738702a16b9
jwin4740@gmail.com <jwin4740@gmail.com>
2022-05-07 22:28:50 -0400

    refactored

--- GenericSelenium.py
+++ GenericSelenium.py
@@ -1,10 +1,34 @@
 from selenium import webdriver
+import selenium
+import os
 
 
 class GenericSelenium:
 
     # TODO: add support for different browsers
     def __init__(self, browser='chrome'):
+        self.options = selenium.webdriver.ChromeOptions()
+        self.options.accept_untrusted_certs = True
+        self.options.assume_untrusted_cert_issuer = True
+        # chrome configuration
+        # More: https://github.com/SeleniumHQ/docker-selenium/issues/89
+        # And: https://github.com/SeleniumHQ/docker-selenium/issues/87
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-impl-side-painting")
+        self.options.add_argument("--disable-setuid-sandbox")
+        self.options.add_argument("--disable-seccomp-filter-sandbox")
+        self.options.add_argument("--disable-breakpad")
+        self.options.add_argument("--disable-client-side-phishing-detection")
+        self.options.add_argument("--disable-cast")
+        self.options.add_argument("--disable-cast-streaming-hw-encoding")
+        self.options.add_argument("--disable-cloud-import")
+        self.options.add_argument("--disable-popup-blocking")
+        self.options.add_argument("--ignore-certificate-errors")
+        self.options.add_argument("--disable-session-crashed-bubble")
+        self.options.add_argument("--disable-ipv6")
+        self.options.add_argument("--allow-http-screen-capture")
+        self.options.add_argument("--start-maximized")
+
         if browser == 'chrome':
             self.driver = webdriver.Chrome(executable_path='drivers/chromedriver')
 
--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from GenericSelenium import GenericSelenium
 
 g = GenericSelenium()
 g.driver.get("https://github.com/login")
-g.login_by_id('jwin4740@gmail.com', '47@Bughouse', 'login_field', 'password')
-g.driver.close()
+g.login_by_id('jwin4740@gmail.com', 'pass', 'login_field', 'password')
+# g.driver.close()
